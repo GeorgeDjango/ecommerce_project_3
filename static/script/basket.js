0d473a51bf5e3766bcc05197afab28594685787c
GeorgeDjangoParker <georgedjangoparkeruk@gmail.com>
2022-08-03 16:28:47 +0100

    made edits to elemnts, and added basket functions

--- static/script/basket.js
+++ static/script/basket.js
@@ -9,8 +9,12 @@ function addBasketTotal() {
 function updateBasket() {
     var basketTotal = document.getElementById('basketTotal'); 
     var quantityInput = document.getElementById('quantityInput');
+    var productName = document.getElementById('productName').innerHTML;
+    var itemPrice = document.getElementById('itemPrice').innerHTML;
     quantity += parseInt(quantityInput.value);
     basketTotal.innerHTML = addBasketTotal();
+    basketName.innerHTML = productName;
+    basketPrice.innerHTML = itemPrice;
 }
 var addToCartBtn = document.getElementById('addToCart');
 
@@ -18,7 +22,13 @@ addToCartBtn.addEventListener('click',updateBasket);
 
 
 
+//const price = 1470000.15;
 
+//let dollarUS = Intl.NumberFormat("en-US", {
+    //style: "currency",
+    //currency: "USD",
+//});
+//console.log("Dollars: " + dollarUS.format(price));
 
 
 
@@ -29,4 +39,16 @@ addToCartBtn.addEventListener('click',updateBasket);
   function decrease() {
     document.getElementById('quantityInput').stepDown();
   }
-  
+
+
+
+
+
+// Get the basket modal
+var cartModal = document.getElementById("myCartModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("showCurrentBasket");
+btn.onclick = function () {
+myCartModal.style.display = "block";
+}
